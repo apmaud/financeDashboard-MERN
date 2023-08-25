e28573c568702c0c45ece152bc6afbd4443b5915
Arjun Maudhoo <apm526@nyu.edu>
2023-08-25 15:31:10 -0500

    Finish KPI routes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js"
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 
 // CONFIGURATIONS
 dotenv.config()
@@ -32,5 +34,11 @@ mongoose
     })
     .then(async () => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+        
+        // ADD DATA ONE TIME ONLY OR AS NEEDED
+        // do NOT ever use in a real production app, for obvious reasons future arjun
+        // await mongoose.connection.db.dropDatabase(); // drop current database, no duplicate database then seed it
+        // KPI.insertMany(kpis);
+
     })
     .catch((error) => console.log(`${error} did not connect`));
--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -5,9 +5,10 @@ const router = express.Router();
 
 router.get("/kpis", async (req,res) => {
     try{
+        // success result of 200 status to the front end
+        // sending our kpis object that we grabbed from database and sending it to front end
         const kpis = await KPI.find();
-        res.status(200).json(kpis)  // success result of 200 status to the front end
-                                    // sending our kpis object that we grabbed from database and sending it to front end
+        res.status(200).json(kpis)
     } catch(error) {
         res.status(404).json({ message: error.message });
     }
